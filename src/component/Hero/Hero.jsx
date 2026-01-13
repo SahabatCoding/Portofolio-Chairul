df93e9ec095f43541c088113f4b5c03573b26f00
Chairul Yusuf <sahabatcoding29@gmail.com>
2026-01-13 07:37:18 +0700

    update all

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -2,23 +2,13 @@ import { getImageUrl } from "../../utils"
 import style from "./Hero.module.css"
 
 export default function Hero() {
-    const downloadFile = () => {
-        const link = document.createElement("a")
-        link.href = getImageUrl("hero/CV.pdf")
-        link.download = "hero/CV.pdf"
-        link.click()
-    };
     return (
         <>
             <section className={style.section} >
                 <div className={style.content}>
-                    <h1>My Name Chairul</h1>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit beatae non provident error ad eum, quam earum harum officia quisquam assumenda perspiciatis incidunt quia alias aliquid sint. Minima, et cum?</p>
-                </div>
-                <div className={style.imageHero}>
-                    <img src={getImageUrl("/hero/profil.png")} alt="profil" className={style.profil} />
-                </div>
-                <div className={style.links}>
+                    <h1>Hi, I'm Chairul</h1>
+                    <p>a Fullstack Developer passionate about building efficient and scalable applications.</p>
+                    <div className={style.links}>
                     <a className={style.download} href={getImageUrl("hero/CV.pdf")} download={"CV_Chairul.pdf"}>Download CV</a>
                     <div className={style.socialMedia}>
                         <a href="">
@@ -32,6 +22,11 @@ export default function Hero() {
                         </a>
                     </div>
                 </div>
+                </div>
+                <div className={style.imageHero}>
+                    <img src={getImageUrl("/hero/profil.png")} alt="profil" className={style.profil} />
+                </div>
+                
             </section>
         </>
     )
